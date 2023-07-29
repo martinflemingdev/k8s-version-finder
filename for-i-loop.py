47ae71b53d52922c36647be7cfe0e97a35409e4b
Martin Fleming <martinfleming@pop-os.localdomain>
2023-07-29 19:31:40 -0400

    Adding for i loop file

---
+++ for-i-loop.py
@@ -0,0 +1,7 @@
+# Python's version of the classic for i loop
+
+my_list = ['v3alpha6', 'v2beta2', 'v1alpha7', 'v3beta4', 'v2alpha1', 'v3beta5', 'v2']
+
+for i in range(len(my_list)):
+    print(f'we are at index: {i}')
+    print(f'the list item at this index is "{my_list[i]}"')
