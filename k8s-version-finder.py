ac98b631d57931ed967736993e6cbaee9796dfb2
Martin Fleming <martinfleming@pop-os.localdomain>
2023-07-29 18:54:22 -0400

    adding challenge

--- k8s-version-finder.py
+++ k8s-version-finder.py
@@ -1,4 +1,10 @@
-# k8s-version-finder
+# in k8s, there are 3 possible version types:
+alpha = ['v1alpha1', 'v2alpha1', 'v2alpha2', 'etc']
+beta = ['v1beta1', 'v1beta2', 'v2beta3', 'etc']
+general_release = ['v1', 'v2', 'v3', 'etc']
+    
+# each version (v#) can have a general release, alpha and beta version
+# v1 is newer than v1beta1, but v1 is older than v2beta1 (because of the "v2")
 
 ###########################################################################################
 # Given a list of versions, write a function that returns the index of the latest version #
@@ -12,3 +18,6 @@
 
 # from newest to oldest, the above versions would be
 # 'v2beta1', 'v2alpha1', v1, 'v1alpha1'
+
+def return_newest_versions(versions):
+    pass
